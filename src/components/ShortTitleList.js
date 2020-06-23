f5d691f067a03a2fc3055e3e35b81bdfccb7849e
mizuki-psy <66736771+mizuki-psy@users.noreply.github.com>
2020-06-23 17:45:11 +0900

    Add files via upload

---
+++ src/components/ShortTitleList.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, graphql } from 'gatsby'
+
+export default class IndexPage extends React.Component {
+  render() {
+    const { posts, title } = this.props
+
+    return (
+          <div>
+            <h1 className="has-text-weight-bold is-size-2">{title}</h1>
+          </div>
+          
+    )
+  }
+}
+
+IndexPage.propTypes = {
+  posts: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string,
+}
+
+export const pageQuery = graphql`
+  fragment PostListFields on wordpress__POST {
+    id
+    title
+    excerpt
+    author {
+      name
+      slug
+      avatar_urls {
+        wordpress_48
+      }
+    }
+    date(formatString: "MMMM DD, YYYY")
+    slug
+    path
+    featured_media {
+      source_url
+    }
+    categories {
+      name
+      slug
+    }
+  }
+`
---
+++ src/components/main.js
@@ -0,0 +1,31 @@
+import React from "react"
+import CategoryList from '../templates/CategoryList'
+import showShortTitles from '../templates/showShortTitles'
+
+const Main = ({ children }) => {
+  return (
+    <main>
+      <section className="section">
+        <div className="container">
+          <div className="columns">
+            <div className="column is-three-quarters content">{children}</div>
+            <div className="column">
+              <div className="has-background-light">
+                最近の投稿一覧
+                <showShortTitles></showShortTitles>
+              </div>
+              <span></span>
+              <div className="has-background-light">
+                カテゴリー
+				<CategoryList></CategoryList>
+              </div>
+              <div className="has-background-light">広告</div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  )
+}
+
+export default Main
