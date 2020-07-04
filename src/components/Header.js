9d521ed38a352abefb1cd154a4593532b31b0a2a
mizuki-psy <66736771+mizuki-psy@users.noreply.github.com>
2020-07-04 20:34:04 +0900

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -49,9 +49,6 @@ const Header = ( { location, title, summary, description } ) => {
 	            <Link to="/categories/z-3/" className="navbar-item">
 	              自己紹介
 	            </Link>
-              <Link to="/blogSearch/" className="navbar-item">
-                ブログ内検索
-              </Link>
               <Link to="/privacy/" className="navbar-item">
                 プライバシーポリシー
               </Link>
